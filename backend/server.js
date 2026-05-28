8f7e2eea0f4f3ec6e3bf1a69a51fe9bc4c053682
mayankyarnagula-ux <mayankyarnagula@gmail.com>
2026-05-28 14:34:33 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+
+mongoose.connect("mongodb+srv://mayankyarnagula_db_user:Api@hospital.gxwuajf.mongodb.net/?appName=hospital")
+.then(()=>{
+    console.log("db connected")
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(4000,()=>{
+    console.log("server started")
+})
